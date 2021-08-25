8c3bb35d6db47bfdfffb7c207a776e0def99156c
failed-commune <w.hanseul@gmail.com>
2021-08-25 15:52:51 +0900

    feat: 1장 6까지

--- src/__tests__/plays.test.js
+++ src/__tests__/plays.test.js
@@ -1,21 +1,17 @@
-const GitHub = require('../lib/github')
+import { statement } from '../statement'
+import invoices from '../invoices.json'
+import plays from '../plays.json'
 
-describe('Integration with GitHub API', () => {  
-  let github
 
-  beforeAll(() => {
-    github = new GitHub({
-      accessToken: process.env.ACCESS_TOKEN,
-      baseURL: 'https://api.github.com',
-    })
-  })
+const LEGACY_OUTPUT = `Statement for BigCo
+  Hamlet: $650.00 (55 seats)
+  As You Like It: $580.00 (35 seats)
+  Othello: $500.00 (40 seats)
+Amount owed is $1,730.00
+You earned 47 credits
+`
 
-  test('Get a user', async () => {
-    const res = await github.getUser('stunstunstun')
-    expect(res).toEqual(
-      expect.objectContaining({
-        login: 'stunstunstun',
-      })
-    )
-  })
+test('print invoice', async () => {
+  const result = statement(invoices[0], plays)
+  expect(result).toEqual(LEGACY_OUTPUT)
 })
